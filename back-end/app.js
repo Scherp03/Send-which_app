b464d8cd8dc48f85c46583e90135c2ebfe8cad19
luca <luca.carpella@gmail.com>
2024-05-28 12:27:45 +0200

    added server.js ,modified package.json

--- back-end/app.js
+++ back-end/app.js
@@ -1,37 +1,19 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const PORT =  3000;
 
 /* Routes */
 const app = express();
 
-// Example: import userRouter from './routes/user.js'
-
-
 app.use(bodyParser.json());
-
 
 /* Quick check if it's working */
 app.get('/', (req, res) => {
-  res.send('Hello from Homepage.');
+  res.status(200).send('Welcome to homepage!');
 });
 
-/* MongoDB credentials */
-const dbUri = 'mongodb+srv://'+process.env.DB_CREDENTIALS+'@'+process.env.DB_HOST+'/'+process.env.DB_NAME+'?retryWrites=true&w=majority';
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-
-/* Database connection */
-mongoose.connect(dbUri, clientOptions)
-.then(() => {
-    console.log('Connected to mongoDB successfully!');
-    app.listen(PORT, () => {
-        console.log(`Server running on port: http://localhost:${PORT}`);
-    });
-})
-.catch((err) => console.log(err));
-
 /* Swagger setup */
+
+
+
+
+export default app;
