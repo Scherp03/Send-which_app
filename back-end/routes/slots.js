cbdd00cb94d69082ea67156a49f6af16c19e17df
Tommaso <zinitommaso17@gmail.com>
2024-05-28 13:53:40 +0200

    Update slots

--- back-end/routes/slots.js
+++ back-end/routes/slots.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 
 app.get('/available-time', async (req, res) => {
     try {
-      const availableTimeSlots = ['9:00 AM', '9:15 AM', '9:30', '9:45 AM', '10:00 AM','10:15 AM', '10:30', '10:45 AM', '11:00 AM','11:15 AM', '11:30', '11:45 AM', '12:00 AM', '12:15 AM', '12:30', '12:45 AM', '13:00 AM','13:15 AM', '13:30', '13:45 AM', '14:00 AM','14:15 AM', '14:30', '14:45 AM', '15:00 AM', '15:15 AM', '15:30', '15:45 AM', '16:00 AM' ];
+      const availableTimeSlots = ['9:00 AM', '9:15 AM', '9:30', '9:45 AM', '10:00 AM','10:15 AM', '10:30 AM', '10:45 AM', '11:00 AM','11:15 AM', '11:30 AM', '11:45 AM', '12:00 PM', '12:15 PM', '12:30 PM', '12:45 PM', '13:00 PM','13:15 PM', '13:30 PM', '13:45 PM', '14:00 PM','14:15 PM', '14:30 PM', '14:45 PM', '15:00 PM', '15:15 PM', '15:30 PM', '15:45 PM', '16:00 PM' ];
       
       // Fetch booked time slots from the database
       const bookedTimeSlots = await Slot.find({ date: req.query.date });
