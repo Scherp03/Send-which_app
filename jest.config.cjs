07dc1ab57790abad774786cb0c165cdaba0aacf8
luca <luca.carpella@gmail.com>
2024-05-28 12:25:31 +0200

    tests initialized, modified README, added .gitignore

---
+++ jest.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest',  // Transform JavaScript files using babel-jest
+    },
+    testEnvironment: 'node', // Specify the test environment (Node.js in this case)
+};
