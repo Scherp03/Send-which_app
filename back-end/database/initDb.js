6f8de45e73eae5850e3607be775b5deefafb7cc0
luca <luca.carpella@gmail.com>
2024-06-02 11:28:03 +0200

    db initialization

--- back-end/database/initDb.js
+++ back-end/database/initDb.js
@@ -1,23 +1,47 @@
 import User from './schemas/user.js';
 import UserType from './schemas/userType.js';
+import { Permissions, Roles } from '../../shared/userTypeDefinitions.js';
 
-const users = [{}, {}];
-const userTypes = [{}];
+// initialize all collections
+const users = [
+  {
+    firstName: 'mockName',
+    lastName: 'mockSurname',
+    username: 'mockUsername',
+    email: 'mockEmail@something',
+    password: 'mockpswd',
+    userTypes: Roles.USER,
+  },
+  {
+    firstName: 'Luca',
+    lastName: 'Carpella',
+    username: 'scherp03',
+    email: 'luca.carpella@gmail.com',
+    password: 'lessgoski69',
+    userTypes: Roles.ADMIN,
+  },
+];
 
 // define all permissions
+const userPerm = [Permissions.VIEWER];
+const adminPerm = [Permissions.EDITOR];
+
+const userTypes = [
+  { id: Roles.USER, permissions: userPerm },
+  { id: Roles.ADMIN, permissions: adminPerm },
+];
 
 export const initDb = async () => {
-    // reset database
-    User.deleteMany();
-    for (let user of users) {
-        await User.create(user);
-    }
-    console.log('Users collection populated');
-    UserType.deleteMany();
-    for (let userType of userTypes) {
-        await UserType.create(userType);
-    }
-    console.log('UserTypes collection populated');
+  User.deleteMany();
+  for (let user of users) {
+    await User.create(user);
+  }
+  console.log('Users collection populated');
+  UserType.deleteMany();
+  for (let userType of userTypes) {
+    await UserType.create(userType);
+  }
+  console.log('UserTypes collection populated');
 };
 
 export default initDb;
