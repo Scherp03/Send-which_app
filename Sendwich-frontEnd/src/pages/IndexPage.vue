45994768af424a669ff772a6d917f303e8d5314a
Tommaso <zinitommaso17@gmail.com>
2024-06-28 15:31:32 +0200

    Quasar basic view

---
+++ Sendwich-frontEnd/src/pages/IndexPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page padding >
+    <input v-model="message" />
+    <h2>{{message}}</h2>
+    <button class="id" @click=login()>Sign in</button>
+    <button class="id">Sign up</button>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  data(){
+    return{
+      message: "Welcome to Sendwich APP"
+    }
+  },
+
+  methods:{
+    login(){
+
+    },
+    signUp(){
+
+    }
+  }
+});
+</script>
+
+
+<style>
+.id{
+  background-color: redS;
+}
+</style>
