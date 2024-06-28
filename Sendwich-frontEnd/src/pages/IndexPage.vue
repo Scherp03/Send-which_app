329a4a5373d0ae71badc3641bf8ea7d89c9b63ba
Tommaso <zinitommaso17@gmail.com>
2024-06-28 19:31:24 +0200

    Index login and sign up

--- Sendwich-frontEnd/src/pages/IndexPage.vue
+++ Sendwich-frontEnd/src/pages/IndexPage.vue
@@ -1,9 +1,22 @@
 <template>
   <q-page padding >
-    <input v-model="message" />
-    <h2>{{message}}</h2>
-    <button class="id" @click=login()>Sign in</button>
+    <input v-model="message"
+    @keyup.enter="alertMessage"
+    v-autofocus
+    :class="{'error ' : message.length>22}" 
+    :style="errorStyle"
+    ref="messageInput"/>
+    <h2 class="border-gray" v-show="message.length">{{message}}</h2>
+
+    <button class="id" @click=alertMessage>Sign in</button>
+
     <button class="id">Sign up</button>
+
+    <hr>
+
+    <p>LoweCase message : {{ messageLowerCase}}</p>
+ 
+
   </q-page>
 </template>
 
@@ -21,14 +34,63 @@ defineOptions({
     },
     signUp(){
 
+    },
+    
+    alertMessage(){
+      alert(this.message)
     }
+  },
+  computed:{
+    messageLowerCase(){
+      return this.message.toLowerCase()
+    },
+    errorStyle(){
+      if( this.message.length > 22){
+        return 'color: red; backgrond: pink;'
+      }
+    }
+  },
+  directives:{
+    autofocus:{
+      inserted(el){
+        el.focus()
+      }
+    }
+  },
+  beforeCreate(){
+    console.log('before create')
+  },
+  created(){
+    console.log('created')
+  },
+  beforeMount(){
+    console.log('before mount')
+  },
+  mounted(){
+    console.log(this.$refs)
+    
+  },
+  beforeUpdate(){
+    console.log('before update')
+  },
+  updated(){
+    console.log('updatede')
   }
+
 });
 </script>
 
 
 <style>
+
+.border-gray{
+  border: 1px solid gray;
+}
 .id{
   background-color: redS;
+}
+.error{
+  color: red;
+  background-color: pink ;
 }
 </style>
