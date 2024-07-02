1f84b8f599c4580de8bcf2225776136fa941ce1e
luca <luca.carpella@gmail.com>
2024-07-03 00:17:02 +0200

    set up swagger documentation

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 
 /* Routes */
 const app = express();
@@ -18,5 +20,19 @@ app.get('/', (req, res) => {
 });
 
 /* Swagger setup */
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Send-which API',
+      description: "Documentation for Send-which's REST APIs",
+      version: '1.0.0',
+    },
+  },
+  apis: ['./back-end/routes/*.js'],
+};
+
+const swaggerSpecs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 export default app;
--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -4,6 +4,86 @@ import { login, logout } from '../controllers/auth.js';
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * api/v1/auth/login:
+ *   post:
+ *     summary: Authenticate user and return tokens
+ *     description: Login to the application, and returns a bearer access token that is handled by the front end and used throughout every other request
+ *     tags:
+ *      - Authentication
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     requestBody:
+ *       description: The request body is be a JSON object containing the token and the user's id.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: The username of the user
+ *                 example: myusername
+ *               password:
+ *                 type: string
+ *                 description: The password of the user
+ *                 example: mypassword
+ *     responses:
+ *       '200':
+ *         description: The user has provided the correct email and password and is given tokens
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 AccessToken:
+ *                   type: string
+ *                   example: "eyJhbGciOiJIUzI1NiIsInR5cCI7gH8Trdua..."
+ *       '400':
+ *         description: The user has inserted an invalid request, both email and password are required
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "Invalid request. Email and password are required."
+ *       '401':
+ *         description: The user has inserted an invalid password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "Invalid password."
+ *       '404':
+ *         description: The user has inserted an invalid email
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "Account not registered."
+ *       '500':
+ *         description: The server has had some problems during login
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "An error occurred during login."
+ */
 router.post('/login', login);
 
 router.delete('/logout', tokenChecker, logout);
