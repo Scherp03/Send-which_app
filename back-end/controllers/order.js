1ef58cf9dfe3c536f277ef0960db3edcdf152f9a
Niccolò Eccel <niccolo3010.eccel@gmail.com>
2024-07-05 18:25:57 +0200

    Translated all methods to API files

---
+++ back-end/controllers/order.js
@@ -0,0 +1,25 @@
+import Order from '../database/schemas/order.js';
+
+// calculateOrderPrice
+// Calculate the price of an order given its _id
+// Parameters required: _id
+
+export const calculateOrderPrice = async (req, res) => {
+  try {
+    let foundOrder = await Order.findById(req.body._id);
+    if (foundOrder) {
+      let price = await foundOrder.calculatePrice();
+      return res.status(200).json({ success: true, price: price });
+    } else {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Order not found' });
+    }
+  } catch (err) {
+    console.log(err.message);
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
---
+++ back-end/controllers/sandwich.js
@@ -0,0 +1,38 @@
+import Ingredient from '../database/schemas/ingredient.js';
+import Sandwich from '../database/schemas/sandwich.js';
+
+// calculateSandwichPrice
+// Calculate the price of a sandwich given its _id
+// Parameters required: _id
+export const calculateSandwichPrice = async (req, res) => {
+  try {
+    let foundSandwich = await Sandwich.findById(req.body._id);
+    if (foundSandwich) {
+      let price = await foundSandwich.calculatePrice();
+      return res.status(200).json({ success: true, price: price });
+    } else {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Sandwich not found' });
+    }
+  } catch (err) {
+    console.log(err.message);
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+export const findBestSeller = async (req, res) => {
+  try {
+    let bestSeller = await Sandwich.findBestSeller(1);
+    return res.status(200).json({ success: true, bestSeller: bestSeller });
+  } catch (err) {
+    console.log(err.message);
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
