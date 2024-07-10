b4dee20adff1320565f0cdfe639db87faf582ac4
luca <luca.carpella@gmail.com>
2024-07-10 22:08:14 +0200

    code optimized, still need to return token

--- back-end/controllers/oauth.js
+++ back-end/controllers/oauth.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import { OAuth2Client } from 'google-auth-library';
 import fetch from 'node-fetch';
 import UserModel from '../database/schemas/user.js';
+import { Roles } from '../../shared/userTypeDefinitions.js';
 
 dotenv.config();
 
@@ -21,34 +22,38 @@ export const oauth = async (req, res, next) => {
     // Make sure to set the credentials on the OAuth2 client.
     await oAuth2Client.setCredentials(tokens);
 
-    const user = oAuth2Client.credentials;
+    // const user = oAuth2Client.credentials;
     // console.log('credentials', user);
-    await CreateAndLoginUser(oAuth2Client.credentials.access_token, res);
-  } catch (err) {
-    console.log('Error logging in with OAuth2 user', err);
-  }
 
-  res.redirect(303, 'http://localhost:9000/');
-};
+    // await CreateAndLoginUser(oAuth2Client.credentials.access_token);
+    const response = await fetch(
+      `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${oAuth2Client.credentials.access_token}`,
+    );
 
-async function CreateAndLoginUser(access_token, res) {
-  const response = await fetch(
-    `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`,
-  );
+    //console.log('response',response);
+    const userData = await response.json();
+    console.log('data:', userData);
 
-  //console.log('response',response);
-  const userData = await response.json();
-  console.log('data:', userData);
-
-  const user = await UserModel.findOne({ email: userData.email });
-  // if the account was already created, then just login
-  if (user) {
+    const user = await UserModel.findOne({ email: userData.email });
+    // if the account was already created, then just login
+    if (!user) {
+      const data = {
+        firstName: userData.given_name,
+        lastName: userData.family_name,
+        username: userData.given_name + userData.family_name,
+        email: userData.email,
+        password: 'your-new-password',
+        userType: Roles.USER,
+      };
+      const newUser = await UserModel.create(data);
+    }
     const url = 'http://localhost:3000/api/v1/auth/login'; // Replace with your API endpoint
 
+    const logUser = await UserModel.findOne({ email: userData.email });
     // Data to be sent in the request body
     const data = {
-      username: user.username,
-      password: user.password,
+      username: logUser.username,
+      password: logUser.password,
     };
 
     // Configuration for the fetch request
@@ -73,7 +78,7 @@ async function CreateAndLoginUser(access_token, res) {
         // Handle responseData based on your requirements
         if (responseData.success) {
           // Login successful, handle the token or other data
-          // return res.status(200).json({
+          // res.status(200).json({
           //   success: true,
           //   message: responseData.message,
           //   id: responseData.id,
@@ -84,86 +89,13 @@ async function CreateAndLoginUser(access_token, res) {
           throw new Error('Something went wrong while logging the user');
         }
       });
+  } catch (err) {
+    console.log('Error logging in with OAuth2 user', err);
   }
-  // if it is the first time accessing, create the user and then login
-  else {
-    const url = 'http://localhost:3000/api/v1/users'; // Replace with your API endpoint
 
-    // Data to be sent in the request body
-    const data = {
-      firstName: userData.given_name,
-      lastName: userData.family_name,
-      username: userData.given_name + userData.family_name,
-      email: userData.email,
-      password: 'your-new-password',
-    };
+  res.redirect(303, 'http://localhost:9000/');
+};
 
-    // Configuration for the fetch request
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json', // Indicates the content
-      },
-      body: JSON.stringify(data), // Converts data to JSON string
-    };
+// async function CreateAndLoginUser(access_token) {
 
-    // Sending the fetch request
-    fetch(url, requestOptions)
-      .then(async (response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        } else {
-          return response.json(); // Parse the JSON from the response
-        }
-      })
-      .then(async (responseData) => {
-        if (responseData.success) {
-          const url = 'http://localhost:3000/api/v1/auth/login'; // Replace with your API endpoint
-
-          const newUser = await UserModel.findOne({ email: userData.email });
-
-          // Data to be sent in the request body
-          const data = {
-            username: newUser.username,
-            password: newUser.password,
-          };
-
-          // Configuration for the fetch request
-          const requestOptions = {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json', // Indicates the content
-            },
-            body: JSON.stringify(data), // Converts data to JSON string
-          };
-
-          // Sending the fetch request
-          fetch(url, requestOptions)
-            .then((response) => {
-              if (!response.ok) {
-                throw new Error('Network response was not ok');
-              } else {
-                return response.json(); // Parse the JSON from the response
-              }
-            })
-            .then((responseData) => {
-              // Handle responseData based on your requirements
-              if (responseData.success) {
-                // Login successful, handle the token or other data
-                // return res.status(200).json({
-                //   success: true,
-                //   message: responseData.message,
-                //   id: responseData.id,
-                //   token: responseData.token,
-                // });
-              } else {
-                // Login failed
-                throw new Error('Something went wrong while logging the user');
-              }
-            });
-        } else {
-          throw new Error('Something went wrong while creating the user');
-        }
-      });
-  }
-}
+// }
