d9752100d1dfcabc39283a83359a5dd8c9b5ca5e
luca <luca.carpella@gmail.com>
2024-07-15 21:37:40 +0200

    google test fixed consequently

--- back-end/tests/google.test.js
+++ back-end/tests/google.test.js
@@ -12,9 +12,6 @@ afterAll(async () => {
   await mongoose.connection.close();
 }, 20000);
 
-const googleUrl =
-  'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20openid&prompt=consent&response_type=code&client_id=999278747711-tkbd9d81i5nnsml3k46nv8bgen2209bu.apps.googleusercontent.com&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2Foauth';
-
 // Get url to redirect to google
 describe('POST /api/v1/request', () => {
   test('Should respond with 200 status code and a message', async () => {
@@ -23,7 +20,6 @@ describe('POST /api/v1/request', () => {
     expect(response.body).toEqual(
       expect.objectContaining({
         success: true,
-        url: googleUrl,
       }),
     );
   }, 20000);
