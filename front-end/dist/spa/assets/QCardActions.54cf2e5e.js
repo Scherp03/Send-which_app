510ba2960b225650c3f6169213f8e6aeb4c8f4fe
luca <luca.carpella@gmail.com>
2024-07-16 19:27:59 +0200

    app build

---
+++ front-end/dist/spa/assets/QCardActions.54cf2e5e.js
@@ -0,0 +1,1 @@
+import{i as o,at as r,au as c,j as l,k as n,l as i}from"./index.d75b9d65.js";var d=o({name:"QCardActions",props:{...r,vertical:Boolean},setup(a,{slots:s}){const t=c(a),e=l(()=>`q-card__actions ${t.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>n("div",{class:e.value},i(s.default))}});export{d as Q};
